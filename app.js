16a0d6939493dd24a4340d0c3e8017cc3b9f5e5b
Akash <akashrajmane23@gmail.com>
2024-03-14 19:09:34 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const contactRoutes = require("./routes/contact");
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(contactRoutes);
+app.use("/success",(req,res,next)=>{
+    res.send(`<h1>Form filled successfully</h1>`)
+})
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
---
+++ routes/contact.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+const express = require('express');
+
+const router = express.Router();
+
+
+router.get('/contact-us', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../', 'views', 'contact-us.html'));
+});
+
+
+router.post('/contact-us', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/success');
+});
+
+module.exports = router;
